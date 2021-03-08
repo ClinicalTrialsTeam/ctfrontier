34efc311daf75f095fa1a302bcf929acd1599c2c
Balaji Ragupathi <auro@Balajis-MacBook-Pro.local>
2021-03-08 17:04:49 -0500

    addressed mona's review comments. removed commented code from core/settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -46,14 +46,6 @@ INSTALLED_APPS = [
     "ctgov",
 ]
 
-# REST_FRAMEWORK = {
-#     # Use Django's standard `django.contrib.auth` permissions,
-#     # or allow read-only access for unauthenticated users.
-#     "DEFAULT_PERMISSION_CLASSES": [
-#         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-#     ]
-# }
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
