d5b17aec8a66713891ac534ea5bac236f784c0ac
Natalie Mona Matthews <natalie.mona@gmail.com>
2021-03-13 20:21:03 -0500

    fix to follow eslint rules (#4)
    * fix to follow eslint rules
    
    * more linting fixes, eslint precommit, eslint github action
    
    * update github action eslint

--- frontend/src/components/atoms/logos/TopLevelSearchLogo/TopLevelSearchLogo.js
+++ frontend/src/components/atoms/logos/TopLevelSearchLogo/TopLevelSearchLogo.js
@@ -1,13 +1,14 @@
+import React from 'react';
 import './TopLevelSearchLogo.css';
 import logo from './logo.png';
 
 function TopLevelSearchLogo() {
-    return (
-        <div className="ctf-logo-wrapper">
-            <img src={logo} className="ctf-logo" alt="logo" />
-            <span className="ctf-logo"></span>
-        </div>
-    );
+  return (
+    <div className="ctf-logo-wrapper">
+      <img src={logo} className="ctf-logo" alt="logo" />
+      <span className="ctf-logo" />
+    </div>
+  );
 }
 
 export default TopLevelSearchLogo;
