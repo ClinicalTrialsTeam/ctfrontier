4635cc1f0e94fdc6245aadccd1d2cfb59a70e4f9
Natalie Mona <natalie.mona@gmail.com>
2021-03-15 08:31:17 -0400

    remove app.js boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+// Import libraries
+import React, { Component } from 'react';
 import './App.css';
-import project from './apis/project';
 
-const App = () => {
-  const [exampleInfo, setExampleInfo] = useState([]);
+// Import local files
+import TopLevelSearchView from './views/TopLevelSearchView/TopLevelSearchView';
 
-  const fetchExample = async () => {
-    try {
-      const response = await project.get('brief_summaries');
-      setExampleInfo(response.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    fetchExample();
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-        <h1>Example API Response</h1>
-        <h2>{exampleInfo.length}</h2>
-        {console.log(exampleInfo)}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TopLevelSearchView />
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
