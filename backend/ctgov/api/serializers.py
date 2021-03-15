c42cda23c0d73e7e2707001f114701a8406925d1
bragu <kannanbalaji_r@yahoo.com>
2021-03-15 00:01:07 -0400

    Feature/api (#6)
    * Added Basic Search API and Countries API. Created basic_search view and updated models.py to include models for views
    
    * Adding reformatted api/views.py
    
    * Implemented Mona's review comments. Created new README.md file for api.
    
    * Added backticks for the dictionaries in api/README.md. Removed comments from views.py

--- backend/ctgov/api/serializers.py
+++ backend/ctgov/api/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
-from ctgov.models import BriefSummaries
+from ctgov.models import BriefSummaries, BasicSearch, Studies, Facilities
 
 
 class BriefSummariesSerializer(serializers.ModelSerializer):
     class Meta:
         model = BriefSummaries
         fields = ["nct", "description"]
+
+
+class BasicSearchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BasicSearch
+        fields = [
+            "status",
+            "brief_title",
+            "nct_id",
+            "condition_name",
+            "intervention_name",
+            "location_name",
+        ]
+
+
+class CountriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Facilities
+        fields = ["country"]
