f20ffec7aab5046d819e1496938c3fcc2ec51b4e
Natalie Mona <natalie.mona@gmail.com>
2021-03-14 20:33:22 -0400

    cdk

--- cdk/lib/stack.py
+++ cdk/lib/stack.py
@@ -1,8 +1,9 @@
-from aws_cdk import core
+from aws_cdk import core, aws_iam as iam
 
 
 class CtStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
