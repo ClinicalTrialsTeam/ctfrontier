2d7af852305553904550c9010721de83a48a34cf
Balaji Ragupathi <kannanbalaji_r@yahoo.com>
2021-03-23 23:55:04 -0400

    Removed Index from documents.py to avoid F811

--- backend/ctgov/documents.py
+++ backend/ctgov/documents.py
@@ -1,4 +1,4 @@
-from django_elasticsearch_dsl import Document, Index, fields
+from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 from elasticsearch_dsl import analyzer, tokenizer
 from ctgov.models import Documents
