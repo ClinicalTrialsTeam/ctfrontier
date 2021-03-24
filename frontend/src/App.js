42f76b1220bb20bbf60a416df52402593caa5c8a
Elena Arens <elarens@gmail.com>
2021-03-24 11:45:41 +0800

    added layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,47 @@
 // Import libraries
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './App.css';
 
 // Import local files
+import TrialsLayout from './components/organisms/Layout/Layout';
 import SearchView from './components/views/SearchView/SearchView';
 import TrialView from './components/views/TrialView/TrialView';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <main>
-          <Switch>
-            <Route path="/" component={SearchView} exact />
-            <Route path="/trials" component={TrialView} exact />
-          </Switch>
-        </main>
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <main>
+        <Switch>
+          <RouteWrapper path="/" component={SearchView} layout={TrialsLayout} exact />
+          <RouteWrapper path="/trials" component={TrialView} layout={TrialsLayout} exact />
+        </Switch>
+      </main>
+    </BrowserRouter>
+  );
 }
+
+function RouteWrapper({
+  component: Component,
+  layout: Layout,
+}) {
+  return (
+    <Route
+      render={() => {
+        return (
+          <Layout>
+            <Component />
+          </Layout>
+        );
+      }}
+    />
+  );
+}
+
+RouteWrapper.propTypes = {
+  component: PropTypes.array.isRequired,
+  layout: PropTypes.array.isRequired,
+};
 
 export default App;
---
+++ frontend/src/components/organisms/Layout/Layout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Layout } from 'antd';
+import PropTypes from 'prop-types';
+import CommonLogo from '../../atoms/logos/CommonLogo/CommonLogo';
+
+import './Layout.css';
+import 'antd/dist/antd.css';
+
+const { Footer, Content } = Layout;
+
+const TrialsLayout = ({ children }) => {
+  return (
+    <Layout className="layout" style={{ backgroundColor: 'white' }}>
+      <CommonLogo />
+      <Content id="content-bg">
+        {children}
+      </Content>
+      <Footer style={{ textAlign: 'center', backgroundColor: 'white' }}>Clinical Trials Frontier ©2021</Footer>
+    </Layout>
+  );
+};
+
+TrialsLayout.propTypes = {
+  children: PropTypes.array.isRequired,
+};
+
+export default TrialsLayout;
