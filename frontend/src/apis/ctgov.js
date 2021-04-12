331975a32cee52dbde43969870e137d07b81a6f1
Natalie Mona <natalie.mona@gmail.com>
2021-04-11 21:52:13 -0400

    api base url as environment variables

--- frontend/src/apis/ctgov.js
+++ frontend/src/apis/ctgov.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import rateLimit from 'axios-rate-limit';
+import urljoin from 'url-join';
 
 axios.defaults.withCredentials = true;
 
 const http = rateLimit(
   axios.create({
-    baseURL: 'http://localhost:8000/ctgov/api',
+    baseURL: urljoin(process.env.REACT_APP_API_BASE_URL, 'ctgov/api'),
   }),
   { maxRPS: 2 }
 );
