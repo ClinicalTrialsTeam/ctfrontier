1c785d07636bd09fbfd18c88ffe433faad5348a4
Natalie Mona <natalie.mona@gmail.com>
2021-04-17 18:23:49 -0400

    database work in progress

--- cdk/lib/database.py
+++ cdk/lib/database.py
@@ -1,38 +1,38 @@
-from aws_cdk import (
-    core,
-    aws_rds as rds,
-    aws_ec2 as ec2,
-)
-from . import names
+# from aws_cdk import (
+#     core,
+#     aws_rds as rds,
+#     aws_ec2 as ec2,
+# )
+# from . import names
 
 
-class CtfDatabase(core.Construct):
-    def __init__(
-        self,
-        scope: core.Construct,
-        vpc,
-    ):
-        super().__init__(scope, id)
+# class CtfDatabase(core.Construct):
+#     def __init__(
+#         self,
+#         scope: core.Construct,
+#         vpc,
+#     ):
+#         super().__init__(scope, id)
 
-        # subnets?
-        # s3 import buckets?
-        rds.DatabaseInstance(
-            self,
-            "PostgresDatabase",
-            database_name=names.DATABASE,
-            instance_identifier=names.DATABASE_INSTANCE,
-            engine=rds.DatabaseInstanceEngine.POSTGRES,
-            instance_type=ec2.InstanceType.of(
-                ec2.InstanceClass.T2,
-                ec2.InstanceSize.MICRO,
-            ),
-            vpc=vpc,
-            vpc_subnets={"subnet_type": ec2.SubnetType.PRIVATE},
-            storage_encrypted=True,
-            allocated_storage=20,
-            backup_retention=core.Duration.days(1),
-            removal_policy=core.RemovalPolicy.SNAPSHOT,
-        )
+#         # subnets?
+#         # s3 import buckets?
+#         rds.DatabaseInstance(
+#             self,
+#             "PostgresDatabase",
+#             database_name=names.DATABASE,
+#             instance_identifier=names.DATABASE_INSTANCE,
+#             engine=rds.DatabaseInstanceEngine.POSTGRES,
+#             instance_type=ec2.InstanceType.of(
+#                 ec2.InstanceClass.T2,
+#                 ec2.InstanceSize.MICRO,
+#             ),
+#             vpc=vpc,
+#             vpc_subnets={"subnet_type": ec2.SubnetType.PRIVATE},
+#             storage_encrypted=True,
+#             allocated_storage=20,
+#             backup_retention=core.Duration.days(1),
+#             removal_policy=core.RemovalPolicy.SNAPSHOT,
+#         )
 
 
 ##         new DatabaseInstance(this, 'mysql-rds-instance', {
