ed3dca9226142e9f34b4d0a950273291e5d07830
Elena Arens <elarens@gmail.com>
2021-04-21 17:58:11 +0800

    minor changes

--- frontend/src/variables/DummyVizData.js
+++ frontend/src/variables/DummyVizData.js
@@ -1,5 +1,5 @@
 const timelineData = [
-  { nct_id: 'NCT04218877', dates: ['2020-03-10', '2020-07-10'], phase: '' },
+  { nct_id: 'NCT04218877', dates: ['2020-03-10', '2020-07-10'], phase: 'Phase 1' },
   { nct_id: 'NCT04307862', dates: ['2019-12-29', '2021-12-31'], phase: 'Phase 2' },
   { nct_id: 'NCT04444726', dates: ['2019-01-20', '2020-11-20'], phase: 'Not Applicable' },
   { nct_id: 'NCT04435392', dates: ['2020-10-14', '2022-12-01'], phase: 'Phase 1 Phase 2' },
@@ -7,7 +7,7 @@ const timelineData = [
   { nct_id: 'NCT04110873', dates: ['2018-07-27', '2019-06-25'], phase: 'Phase 2' },
   { nct_id: 'NCT04715087', dates: ['2021-02-15', '2021-10-15'], phase: 'Not Applicable' },
   { nct_id: 'NCT03796676', dates: ['2019-02-18', '2020-04-08'], phase: 'Phase 3' },
-  { nct_id: 'NCT04213355', dates: ['2020-03-10', '2020-07-10'], phase: '' },
+  { nct_id: 'NCT04213355', dates: ['2020-03-10', '2020-07-10'], phase: 'Not Applicable' },
   { nct_id: 'NCT04306062', dates: ['2019-12-29', '2021-12-31'], phase: 'Phase 2' },
   { nct_id: 'NCT04444726', dates: ['2019-01-20', '2020-11-20'], phase: 'Phase 4' },
   { nct_id: 'NCT04555392', dates: ['2020-10-14', '2022-12-01'], phase: 'Phase 1 Phase 2' },
