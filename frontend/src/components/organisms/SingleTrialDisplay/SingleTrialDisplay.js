4fa0fbf3d90092a73f671a884fe41e1212d045b7
Elena Arens <elarens@gmail.com>
2021-04-24 17:11:41 +0800

    single trial view interface update

--- frontend/src/components/organisms/SingleTrialDisplay/SingleTrialDisplay.js
+++ frontend/src/components/organisms/SingleTrialDisplay/SingleTrialDisplay.js
@@ -1,12 +1,84 @@
 import React, { Component } from 'react';
 import {
-  Collapse, Typography, Row, Col,
+  Collapse, Typography, Row, Col, Descriptions, Divider,
 } from 'antd';
+import { fields } from './SingleTrialDisplayConfig';
+
+// import { fields } from './SingleTrialDisplayConfig';
 
 class SingleTrialDisplay extends Component {
   render() {
     const { Panel } = Collapse;
     const { Title } = Typography;
+
+    const leftSummaryDescriptions = [];
+    const rightSummaryDescriptions = [];
+    const trackingInformation = [];
+    const descriptiveInformation = [];
+    const recruitmentInformation = [];
+    const administrativeInformation = [];
+
+    const leftSummaryKeys = ['sponsor', 'studyCollaborator', 'responsibleParty'];
+    const rightSummaryKeys = ['status', 'resultsFirstPostedDate', 'lastUpdatePostedDate'];
+    const trackingInformationKeys = [
+      'firstSubmittedDate',
+      'resultsFirstPostedDate',
+      'lastUpdatePostedDate',
+      'studyStartDate',
+      'primaryCompletionDate',
+      'studyOutcomeMeasuresCurrentPrimary',
+      'studyOutcomeMeasuresOriginalPrimary',
+      'studyOutcomeMeasuresCurrentSecondary',
+      'studyOutcomeMeasuresOriginalSecondary',
+    ];
+    const descriptiveInformationKeys = [
+      'title',
+      'briefTitle',
+      'summary',
+      'detailedDescription',
+      'type',
+      'design',
+      'sampling',
+      'population',
+      'condition',
+      'subcondition',
+      'intervention',
+    ];
+    const recruitmentInformationKeys = [
+      'age',
+      'sex',
+      'ethnicity',
+      'healthy',
+    ];
+    const administrativeInformationKeys = [
+      'studyIds',
+      'studyFunderType',
+      'studyDocumentType',
+    ];
+
+    const descriptionsArrays = [
+      [leftSummaryKeys, leftSummaryDescriptions],
+      [rightSummaryKeys, rightSummaryDescriptions],
+      [trackingInformationKeys, trackingInformation],
+      [descriptiveInformationKeys, descriptiveInformation],
+      [recruitmentInformationKeys, recruitmentInformation],
+      [administrativeInformationKeys, administrativeInformation],
+    ];
+
+    descriptionsArrays.forEach((element) => {
+      Array.from(element[0].entries()).forEach(([, value]) => {
+        const title = fields[value];
+        element[1].push(
+          <Descriptions.Item
+            key={value}
+            span={4}
+            label={title}
+          >
+            Placeholder text placeholder text placeholder text
+          </Descriptions.Item>
+        );
+      });
+    });
 
     return (
       <>
@@ -14,38 +86,52 @@ class SingleTrialDisplay extends Component {
           Does Timeliness of DTaP-IPV-Hib Vaccination
           Affect Development of Atopic Dermatitis Before 1 Year of Age?
         </Title>
-        <Row key="row-summary" gutter={[8, 8]}>
+        <Divider orientation="left">ClinicalTrials.gov Identifier: </Divider>
+        <Row key="row-summary" gutter={[16, 8]}>
           <Col key="col-sum-1" span={12}>
-            <p />
+            <Descriptions bordered size="small" width="100px">
+              {leftSummaryDescriptions}
+            </Descriptions>
           </Col>
-          <Col key="col-sum-2" span={2}>
-            <p id="or-p">OR</p>
+          <Col key="col-sum-2" span={12}>
+            <Descriptions bordered size="small">
+              {rightSummaryDescriptions}
+            </Descriptions>
           </Col>
         </Row>
+        <br />
         <Collapse defaultActiveKey={['1']}>
           <Panel
             key="panel-tracking"
             header="Tracking information"
           >
-            <p />
+            <Descriptions bordered size="small">
+              {trackingInformation}
+            </Descriptions>
           </Panel>
           <Panel
             key="2"
             header="Descriptive information"
           >
-            <p />
+            <Descriptions bordered size="small">
+              {descriptiveInformation}
+            </Descriptions>
           </Panel>
           <Panel
             key="3"
             header="Recruitment information"
           >
-            <p />
+            <Descriptions bordered size="small">
+              {recruitmentInformation}
+            </Descriptions>
           </Panel>
           <Panel
             key="4"
             header="Administrative information"
           >
-            <p />
+            <Descriptions bordered size="small">
+              {administrativeInformation}
+            </Descriptions>
           </Panel>
         </Collapse>
       </>
