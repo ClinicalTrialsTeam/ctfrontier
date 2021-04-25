c3a9a0ac175c80761b8feab1a139e5f93d37a9be
Elena Arens <elarens@gmail.com>
2021-04-25 15:31:20 +0800

    added functionality of downloading search results csv

--- frontend/src/components/molecules/downloads/TrialsCSVDownload/TrialsCSVDownload.js
+++ frontend/src/components/molecules/downloads/TrialsCSVDownload/TrialsCSVDownload.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button } from 'antd';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import FileDownload from 'js-file-download';
 import image from './icon-csv.png';
 
-const TrialsCSVDownload = () => {
-  return (
-    <>
-      <img className="download-icon" src={image} alt="icon" />
-      <Button className="download-button" type="primary">Save Search Results</Button>
-    </>
-  );
-};
+class TrialsCSVDownload extends Component {
+  static downloadSearchConfig(data) {
+    const replacer = (key, value) => { return (value === null ? '' : value); }; // specifying handling null values here
+    const header = Object.keys(data[0]); // defining header row
+    const csv = [
+      header.join(','), // header row first
+      ...data.map((row) => { return header.map((fieldName) => { return JSON.stringify(row[fieldName], replacer); }).join(','); }),
+    ].join('\r\n');
 
-// TrialsCSVDownload.propTypes = {
-//   data: PropTypes.array.isRequired,
+    console.log(csv);
+    FileDownload(csv, 'trials.csv');
+  }
+
+  render() {
+    const {
+      data,
+    } = this.props;
+
+    return (
+      <>
+        <img className="download-icon" src={image} alt="icon" />
+        <Button
+          className="download-button"
+          onClick={() => { return TrialsCSVDownload.downloadSearchConfig(data.search_results); }}
+          type="primary"
+        >
+          Save Search Results
+        </Button>
+      </>
+    );
+  }
+}
+
+// const TrialsCSVDownload = () => {
+//   return (
+//     <>
+//       <img className="download-icon" src={image} alt="icon" />
+//       <Button className="download-button" type="primary">Save Search Results</Button>
+//     </>
+//   );
 // };
+
+TrialsCSVDownload.propTypes = {
+  data: PropTypes.object.isRequired,
+};
 
 export default TrialsCSVDownload;
