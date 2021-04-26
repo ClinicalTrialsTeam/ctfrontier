b4b6eadfabae32fd7b84803a766680f31a949da8
bragu <kannanbalaji_r@yahoo.com>
2021-04-26 13:20:29 -0400

    Along with minor enhancements, added Search Results Export API, Single Study Detail API, Trial Timelines API and Trial Dashboard API. Updated backend/README.md and View definition. (#56)
    * Added Search Results Export API, Single Study Detail API, Trial Timelines API and Trial Dashboard API. Updated backend/README.md accordingly.
    
    * Updated view definition in database/scripts/search_studies.sql to account for additional parameters city and state

--- backend/ctgov/api/urls.py
+++ backend/ctgov/api/urls.py
@@ -3,10 +3,25 @@ from .views import (
     BriefSummariesListApiView,
     SearchStudiesApiView,
     CountriesListApiView,
+    ConditionsListApiView,
+    StatesListApiView,
+    CitiesListApiView,
+    StudyDetailApiView,
+    TrialsDashboardApiView,
+    SearchResultsExportApiView,
+    TrialTimelinesApiView,
 )
+
 
 urlpatterns = [
     path("brief_summaries", BriefSummariesListApiView.as_view()),
     path("search_studies", SearchStudiesApiView.as_view()),
     path("countries", CountriesListApiView.as_view()),
+    path("conditions", ConditionsListApiView.as_view()),
+    path("states", StatesListApiView.as_view()),
+    path("cities", CitiesListApiView.as_view()),
+    path("study_detail", StudyDetailApiView.as_view()),
+    path("trials_dashboard", TrialsDashboardApiView.as_view()),
+    path("search_results_export", SearchResultsExportApiView.as_view()),
+    path("trial_timelines", TrialTimelinesApiView.as_view()),
 ]
