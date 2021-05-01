401401ce9618848f4645b11936d7d62332e150d6
elarens <31107130+elarens@users.noreply.github.com>
2021-05-01 23:03:50 +0800

    Feature/fe/single trial page with data (#66)
    * intermediate updates
    
    * added data to single trial page
    
    * bug fixes
    
    * add more logging, hard code api base url in dev docker
    
    Co-authored-by: Natalie Mona <natalie.mona@gmail.com>

--- frontend/src/components/organisms/ListViewTable/ListViewTable.js
+++ frontend/src/components/organisms/ListViewTable/ListViewTable.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import {
-  Table, Space, Button, Row, Col, Card, Tooltip,
+  Table, Space, Button, Row, Col, Card, Tooltip, Typography,
 } from 'antd';
 import PropTypes from 'prop-types';
 import {
@@ -20,7 +20,6 @@ import {
   funder, documents, submission,
 } from '../../../variables/SelectOptionsData';
 
-import { columns } from './ListViewTableConfig';
 import './ListViewTable.css';
 
 class ListViewTable extends Component {
@@ -45,14 +44,7 @@ class ListViewTable extends Component {
       searchData: this.props.history.location.state.data,
       payload: this.props.history.location.state.payload,
       dashboardData: {},
-      facetsHeight: 0,
     };
-  }
-
-  componentDidMount() {
-    const facetsHeight = this.facetsRef.current.offsetHeight;
-    this.setState({ facetsHeight });
-    console.log(this.state.facetsHeight);
   }
 
   handleClear() {
@@ -125,6 +117,76 @@ class ListViewTable extends Component {
   }
 
   render() {
+    const { Link } = Typography;
+    const columns = [
+      {
+        title: 'NCT ID',
+        dataIndex: 'nct_id',
+        key: 'nct_id',
+        fixed: 'left',
+        width: 120,
+        sortDirections: ['descend', 'ascend'],
+        sorter: (a, b) => { return a.nct_id.localeCompare(b.nct_id); },
+        render: (nctId) => {
+          return (
+            <Link
+              type="link"
+              size="small"
+              href={'./trials/' + nctId}
+            >
+              {nctId}
+            </Link>
+          );
+        },
+      },
+      {
+        title: 'Brief Title',
+        dataIndex: 'brief_title',
+        key: 'brief_title',
+        width: 250,
+      },
+      {
+        title: 'Condition',
+        dataIndex: 'condition_name',
+        key: 'condition_name',
+        width: 150,
+        sortDirections: ['descend', 'ascend'],
+        sorter: (a, b) => {
+          return a.condition_name.localeCompare(b.condition_name);
+        },
+      },
+      {
+        title: 'Intervention',
+        dataIndex: 'intervention_name',
+        key: 'intervention_name',
+        sorter: true,
+        width: 200,
+      },
+      {
+        title: 'Sponsor',
+        dataIndex: 'sponsor_name',
+        key: 'sponsor_name',
+        sortDirections: ['descend', 'ascend'],
+        sorter: (a, b) => { return a.sponsor_name.localeCompare(b.sponsor_name); },
+        width: 150,
+      },
+      {
+        title: 'Phase',
+        dataIndex: 'study_phase',
+        key: 'study_phase',
+        sortDirections: ['descend', 'ascend'],
+        sorter: (a, b) => { return a.study_phase.localeCompare(b.study_phase); },
+        width: 100,
+      },
+      {
+        title: 'Status',
+        dataIndex: 'status',
+        key: 'status',
+        sortDirections: ['descend', 'ascend'],
+        sorter: (a, b) => { return a.status.localeCompare(b.status); },
+        width: 100,
+      },
+    ];
     const { data } = this.props.history.location.state;
     const dataCount = parseInt(data.metadata[0].results_count);
     const parsedResults = data.search_results.map((result) => {
--- frontend/src/components/organisms/SingleTrialDisplay/SingleTrialDisplay.js
+++ frontend/src/components/organisms/SingleTrialDisplay/SingleTrialDisplay.js
@@ -1,63 +1,112 @@
+/* eslint-disable react/no-unused-state */
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import {
   Collapse, Typography, Row, Col, Descriptions, Divider,
 } from 'antd';
-import { fields } from './SingleTrialDisplayConfig';
-
-// import { fields } from './SingleTrialDisplayConfig';
+import PropTypes from 'prop-types';
+import ctgov from '../../../apis/ctgov';
 
 class SingleTrialDisplay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      acronym: { title: 'Acronym', value: '...' },
+      brief_title: { title: 'Brief Title', value: '...' },
+      condition_name: { title: 'Condition', value: '...' },
+      country_name: { title: 'Country', value: '...' },
+      document_types: { title: 'Document Types', value: '...' },
+      eligibility_criteria: { title: 'Eligibility Criteria', value: '...' },
+      eligibility_gender: { title: 'Gender', value: '...' },
+      eligibility_max_age: { title: 'Maximum Age', value: '...' },
+      eligibility_min_age: { title: 'Minimum Age', value: '...' },
+      funder_type: { title: 'Funder Type', value: '...' },
+      healthy_volunteers: { title: 'Accepts Healty Volunteers', value: '...' },
+      intervention_name: { title: 'Intervention', value: '...' },
+      is_unapproved_device: { title: 'Is Unapproved Device', value: '...' },
+      last_update_posted_date: { title: 'Last Update Posted', value: '...' },
+      location_name: { title: 'Location', value: '...' },
+      nct_id: { title: 'NCT Number', value: '...' },
+      official_title: { title: 'Official Title', value: '...' },
+      primary_completion_date: { title: 'Primary Completion Date', value: '...' },
+      primary_outcome_measures: { title: 'Primary Outcome Measures', value: '...' },
+      results_first_posted_date: { title: 'Results First Posted', value: '...' },
+      results_submitted_qc_done: { title: 'Results Submitted QC Done', value: '...' },
+      results_submitted_qc_not_done: { title: 'Results Submitted QC Not Done', value: '...' },
+      secondary_outcome_measures: { title: 'Secondary Outcome Measures', value: '...' },
+      sponsor_name: { title: 'Sponsor', value: '...' },
+      state_name: { title: 'State', value: '...' },
+      status: { title: 'Recruitment Status', value: '...' },
+      study_brief_desc: { title: 'Brief Description', value: '...' },
+      study_detailed_desc: { title: 'Detailed Description', value: '...' },
+      study_first_posted_date: { title: 'First Posted', value: '...' },
+      study_ids: { title: 'Study IDs', value: '...' },
+      study_phase: { title: 'Phase', value: '...' },
+      study_start_date: { title: 'Start Date', value: '...' },
+      study_type: { title: 'Study Type', value: '...' },
+    };
+  }
+
+  async componentDidMount() {
+    const nctId = this.props.location.pathname.split('/').slice(-1)[0];
+    try {
+      const nct = { nct_id: nctId };
+      const response = await ctgov.post('study_detail', nct);
+      Object.entries(response.data[0]).forEach(
+        ([key, value]) => {
+          const { title } = this.state[key];
+          this.setState({ [key]: { title, value } });
+        }
+      );
+    } catch (err) {
+      // City name is ignored and generates an inconsequential error
+      console.log(err);
+    }
+  }
+
   render() {
     const { Panel } = Collapse;
     const { Title } = Typography;
 
-    const leftSummaryDescriptions = [];
     const rightSummaryDescriptions = [];
     const trackingInformation = [];
     const descriptiveInformation = [];
     const recruitmentInformation = [];
     const administrativeInformation = [];
 
-    const leftSummaryKeys = ['sponsor', 'studyCollaborator', 'responsibleParty'];
-    const rightSummaryKeys = ['status', 'resultsFirstPostedDate', 'lastUpdatePostedDate'];
+    const rightSummaryKeys = ['sponsor_name', 'status', 'study_first_posted_date', 'last_update_posted_date'];
     const trackingInformationKeys = [
-      'firstSubmittedDate',
-      'resultsFirstPostedDate',
-      'lastUpdatePostedDate',
-      'studyStartDate',
-      'primaryCompletionDate',
-      'studyOutcomeMeasuresCurrentPrimary',
-      'studyOutcomeMeasuresOriginalPrimary',
-      'studyOutcomeMeasuresCurrentSecondary',
-      'studyOutcomeMeasuresOriginalSecondary',
+      'results_first_posted_date',
+      'last_update_posted_date',
+      'study_start_date',
+      'primary_completion_date',
+      'primary_outcome_measures',
+      'secondary_outcome_measures',
     ];
     const descriptiveInformationKeys = [
-      'title',
-      'briefTitle',
-      'summary',
-      'detailedDescription',
-      'type',
-      'design',
-      'sampling',
-      'population',
-      'condition',
-      'subcondition',
-      'intervention',
+      'official_title',
+      'brief_title',
+      'study_brief_desc',
+      'study_detailed_desc',
+      'study_type',
+      'condition_name',
+      'intervention_name',
     ];
     const recruitmentInformationKeys = [
-      'age',
-      'sex',
-      'ethnicity',
-      'healthy',
+      'eligibility_min_age',
+      'eligibility_max_age',
+      'eligibility_gender',
+      'healthy_volunteers',
+      'eligibility_criteria',
+      'study_phase',
     ];
     const administrativeInformationKeys = [
-      'studyIds',
-      'studyFunderType',
-      'studyDocumentType',
+      'study_ids',
+      'funder_type',
+      'document_types',
     ];
 
     const descriptionsArrays = [
-      [leftSummaryKeys, leftSummaryDescriptions],
       [rightSummaryKeys, rightSummaryDescriptions],
       [trackingInformationKeys, trackingInformation],
       [descriptiveInformationKeys, descriptiveInformation],
@@ -67,14 +116,18 @@ class SingleTrialDisplay extends Component {
 
     descriptionsArrays.forEach((element) => {
       Array.from(element[0].entries()).forEach(([, value]) => {
-        const title = fields[value];
+        const { title } = this.state[value];
+        let content = this.state[value].value;
+        if (!content) {
+          content = 'Not Available';
+        }
         element[1].push(
           <Descriptions.Item
             key={value}
             span={4}
             label={title}
           >
-            Placeholder text placeholder text placeholder text
+            {content}
           </Descriptions.Item>
         );
       });
@@ -83,24 +136,22 @@ class SingleTrialDisplay extends Component {
     return (
       <>
         <Title level={4}>
-          Does Timeliness of DTaP-IPV-Hib Vaccination
-          Affect Development of Atopic Dermatitis Before 1 Year of Age?
+          {this.state.brief_title.value}
         </Title>
-        <Divider orientation="left">ClinicalTrials.gov Identifier: </Divider>
+        <Divider orientation="left">
+          ClinicalTrials.gov Identifier:
+          {' '}
+          {this.state.nct_id.value}
+        </Divider>
         <Row key="row-summary" gutter={[16, 8]}>
-          <Col key="col-sum-1" span={12}>
-            <Descriptions bordered size="small" width="100px">
-              {leftSummaryDescriptions}
-            </Descriptions>
-          </Col>
-          <Col key="col-sum-2" span={12}>
+          <Col key="col-sum-2" span={24}>
             <Descriptions bordered size="small">
               {rightSummaryDescriptions}
             </Descriptions>
           </Col>
         </Row>
         <br />
-        <Collapse defaultActiveKey={['1']}>
+        <Collapse defaultActiveKey={['panel-tracking']}>
           <Panel
             key="panel-tracking"
             header="Tracking information"
@@ -110,7 +161,7 @@ class SingleTrialDisplay extends Component {
             </Descriptions>
           </Panel>
           <Panel
-            key="2"
+            key="panel-descriptive"
             header="Descriptive information"
           >
             <Descriptions bordered size="small">
@@ -118,7 +169,7 @@ class SingleTrialDisplay extends Component {
             </Descriptions>
           </Panel>
           <Panel
-            key="3"
+            key="panel-recruitment"
             header="Recruitment information"
           >
             <Descriptions bordered size="small">
@@ -126,7 +177,7 @@ class SingleTrialDisplay extends Component {
             </Descriptions>
           </Panel>
           <Panel
-            key="4"
+            key="panel-administrative"
             header="Administrative information"
           >
             <Descriptions bordered size="small">
@@ -139,4 +190,8 @@ class SingleTrialDisplay extends Component {
   }
 }
 
-export default SingleTrialDisplay;
+SingleTrialDisplay.propTypes = {
+  location: PropTypes.object.isRequired,
+};
+
+export default withRouter(SingleTrialDisplay);
